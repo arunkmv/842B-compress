5fab1a591555257aa3f671f06291c77cfcd88d9a
Arun <kumar.mvarun.arun@gmail.com>
2020-03-25 12:58:25 +0530

    Add. CPU byte order/endianness computation

--- compressor/CompressorConfig.cpp
+++ compressor/CompressorConfig.cpp
@@ -2,7 +2,7 @@
 
 compress::CompressorConfig::CompressorConfig() {
     this->blockSize = 32;                       //Block size in bytes
-    this->subBlockCount = blockSize/8;
+    this->subBlockCount = blockSize / 8;
     this->hashTableSize[0] = 32;
     this->hashTableSize[1] = 64;
     this->hashTableSize[2] = 64;
@@ -10,4 +10,11 @@ compress::CompressorConfig::CompressorConfig() {
     this->dictionarySize[1] = 16;
     this->dictionarySize[2] = 16;
     this->hash = compress::HashFunction::BitSubset;
+    this->byteOrder = findEndianness();
+}
+
+bool compress::CompressorConfig::findEndianness() {
+    short int word = 0x0001;
+    char *byte = (char *) &word;
+    return byte[0] != 0;                        //Returns false if CPU is Little Endian and true if Big Endian
 }
--- compressor/CompressorConfig.h
+++ compressor/CompressorConfig.h
@@ -8,11 +8,15 @@ namespace compress {
 
     class CompressorConfig {
     public:
+        CompressorConfig();
+
+        static bool findEndianness();
+
         int blockSize;
         int subBlockCount;
         int hashTableSize[3]{};
         int dictionarySize[3]{};
+        bool byteOrder;
         compress::HashFunction hash;
-        CompressorConfig();
     };
 }
