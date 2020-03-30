7aa2223e1f91da576fbaa94b2fa82c057832d804
Arun <kumar.mvarun.arun@gmail.com>
2020-03-30 18:38:27 +0530

    Fix. Return if match found

--- compressor/HashManager.cpp
+++ compressor/HashManager.cpp
@@ -2,10 +2,10 @@
 
 compress::HashManager::HashManager(compress::CompressorConfig *config, uint64_t *data8, uint32_t *data4,
                                    uint16_t *data2, int *phrase8, int *phrase4, int *phrase2) {
-    this->config  = config;
-    this->data8   = data8;
-    this->data4   = data4;
-    this->data2   = data2;
+    this->config = config;
+    this->data8 = data8;
+    this->data4 = data4;
+    this->data2 = data2;
     this->phrase8 = phrase8;
     this->phrase4 = phrase4;
     this->phrase2 = phrase2;
@@ -14,19 +14,20 @@ compress::HashManager::HashManager(compress::CompressorConfig *config, uint64_t 
 
 bool compress::HashManager::checkTemplate(int op) {
     uint8_t *templateToCheck = templateCombinations[op];
-    int i, match, n = 0;
+    int i, n = 0;
+    bool match;
 
-    if(op >= OPS_MAX) {
+    if (op >= OPS_MAX) {
         return false;
     }
 
-    for(i = 0; i < 4; i++) {
-        if(templateToCheck[i] & OP_ACTION_INDEX) {
-            if(templateToCheck[i] & OP_AMOUNT_2)
+    for (i = 0; i < 4; i++) {
+        if (templateToCheck[i] & OP_ACTION_INDEX) {
+            if (templateToCheck[i] & OP_AMOUNT_2)
                 match = checkIndex(2, n >> 1);
-            else if(templateToCheck[i] & OP_AMOUNT_4)
+            else if (templateToCheck[i] & OP_AMOUNT_4)
                 match = checkIndex(4, n >> 2);
-            else if(templateToCheck[i] & OP_AMOUNT_8)
+            else if (templateToCheck[i] & OP_AMOUNT_8)
                 match = checkIndex(8, 0);
             else
                 return false;
@@ -35,10 +36,12 @@ bool compress::HashManager::checkTemplate(int op) {
         }
         n += templateToCheck[i] & OP_AMOUNT;
     }
+
+    return true;
 }
 
-int compress::HashManager::checkIndex(int i, int index) {
-    return 0;
+bool compress::HashManager::checkIndex(int i, int index) {
+    return false
 }
 
 void compress::HashManager::resetPhrases() {
--- compressor/HashManager.h
+++ compressor/HashManager.h
@@ -3,23 +3,25 @@
 
 namespace compress {
 
-class HashManager {
-public:
-    explicit HashManager(CompressorConfig *config, uint64_t *data8, uint32_t *data4, uint16_t *data2, int *phraseIndex8,
-                         int *phraseIndex4,
-                         int *phraseIndex2);
+    class HashManager {
+    public:
+        explicit HashManager(CompressorConfig *config, uint64_t *data8, uint32_t *data4, uint16_t *data2,
+                             int *phraseIndex8,
+                             int *phraseIndex4,
+                             int *phraseIndex2);
 
-    void resetPhrases();
+        void resetPhrases();
 
-    bool checkTemplate(int op);
+        bool checkTemplate(int op);
 
-private:
-    CompressorConfig *config;
-    uint64_t *data8;
-    uint32_t *data4;
-    uint16_t *data2;
-    int *phrase8, *phrase4, *phrase2;
-    int checkIndex(int i, int index);
-};
+    private:
+        CompressorConfig *config;
+        uint64_t *data8;
+        uint32_t *data4;
+        uint16_t *data2;
+        int *phrase8, *phrase4, *phrase2;
+
+        bool checkIndex(int i, int index);
+    };
 
 }
